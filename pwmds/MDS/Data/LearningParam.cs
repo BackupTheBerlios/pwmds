bda83ed764f10837b2a8f38ae277936066793efe
saintpaulia <saintpaulia>
2007-05-10 10:42:58 +0000

    *** empty log message ***

--- pwmds/MDS/Data/LearningParam.cs
+++ pwmds/MDS/Data/LearningParam.cs
@@ -16,6 +16,11 @@ namespace MDS.Data
         
         private int kFoldSamples;
 
+        private bool earlyStop;
+        private double learnSet;
+        private double validateSet;
+        private double testSet;
+
         private List<double[]> input;
         private List<double[]> output;
 
@@ -52,6 +57,30 @@ namespace MDS.Data
         {
             get { return kFoldSamples; }
             set { kFoldSamples = value; }
+        }
+
+        public bool EarlyStop
+        {
+            get { return earlyStop; }
+            set { earlyStop = value; }
+        }
+
+        public double LearnSet
+        {
+            get { return learnSet; }
+            set { learnSet = value; }
+        }
+
+        public double ValidateSet
+        {
+            get { return validateSet; }
+            set { validateSet = value; }
+        }
+
+        public double TestSet
+        {
+            get { return testSet; }
+            set { testSet = value; }
         }
 
         public List<double[]> Input
